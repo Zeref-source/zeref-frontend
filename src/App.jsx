4d58d1a2afd63d8a5f36284164055e4da6497287
Zeref <zeref@gaming.com>
2026-04-23 23:24:03 +0530

    Fix blank screen crash on logo

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <div className="bg-mesh" />
 
       <header className="header">
-        <span className="header-logo" style={{ cursor: 'pointer' }} onClick={goHome}>
+        <span className="header-logo" style={{ cursor: 'pointer' }} onClick={() => navigate('gaming-news')}>
           <svg width="28" height="28" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <linearGradient id="hzGrad" x1="0%" y1="0%" x2="100%" y2="100%">
